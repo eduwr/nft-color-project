47e528e980c7cfa13da255db6b20b2b5ef72095e
eduwr <eduwric@gmail.com>
2021-10-04 23:39:22 -0300

    fix: compiler versions

---
+++ src/contracts/Color.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.6;
+
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Color is ERC721  {
+
+  constructor() ERC721("Color", "CLR") {}
+
+
+
+
+}
