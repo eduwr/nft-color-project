0f1181b2c3c001133dee76cd8197b4de6a81ae79
eduwr <eduwric@gmail.com>
2021-10-05 08:55:15 -0300

    feat: deploy and test the contract

--- test/Color.test.js
+++ test/Color.test.js
@@ -1,13 +1,40 @@
 /* eslint-disable no-undef */
 
+const { assert } = require("chai");
+
 const Color = artifacts.require("./Color");
 
-require("chai")
-  .use(require("chai-as-promised"))
-  .should();
+require("chai").use(require("chai-as-promised")).should();
 
 contract("Color", (accounts) => {
+  let contract;
+
+  before(async () => {
+    contract = await Color.deployed();
+  });
   describe("deployment", async () => {
-    it("Deploy successfully", async () => {});
+    it("Deploy successfully", async () => {
+      const address = contract.address;
+      console.log("Contract Address: ", address);
+      assert.notEqual(address, "");
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+      assert.exists(address);
+    });
+
+    it("should has a name", async () => {
+      const name = await contract.name();
+      console.log("Contract Name: ", name);
+      assert.exists(name);
+      assert.equal(name, "Color");
+    });
+
+    it("should has a symbol", async () => {
+      const symbol = await contract.symbol();
+      console.log("Contract Symbol: ", symbol);
+      assert.exists(symbol);
+      assert.equal(symbol, "CLR");
+    });
   });
 });
