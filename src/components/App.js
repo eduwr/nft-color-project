41c646e69c9eaf65b64182f4897a8f0dc9a3ec13
eduwr <eduwric@gmail.com>
2021-10-05 22:38:07 -0300

    feat: fetch colors

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,111 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Web3 from "web3";
+import color from "../abis/Color.json";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
+const App = () => {
+  const abi = color.abi;
+
+  const [account, setAccount] = useState("");
+  const [contract, setContract] = useState();
+  const [totalSupply, setTotalSupply] = useState();
+  const [colors, setColors] = useState([]);
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  };
+
+  const loadBlockchainData = async () => {
+    const web3 = window.web3;
+    const [acc] = await web3.eth.getAccounts();
+    setAccount(acc);
+
+    const networkId = await web3.eth.net.getId();
+    const networkData = color.networks[networkId];
+
+    if (!networkData) {
+      window.alert("Smart contract not deployed to detected network");
+      return;
+    }
+
+    const contractAddress = networkData.address;
+    const _contract = new web3.eth.Contract(abi, contractAddress);
+    setContract(_contract);
+  };
+
+  useEffect(() => {
+    (async () => {
+      await loadWeb3();
+      await loadBlockchainData();
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (!contract) {
+      return;
+    }
+    const fetchContractData = async () => {
+      const _colors = await contract.methods.getColors().call();
+      setColors(_colors);
+      const _totalSupply = await contract.methods.totalSupply().call();
+      setTotalSupply(_totalSupply);
+    };
+
+    fetchContractData();
+  }, [contract]);
+
+  return (
+    <div>
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          href="http://www.dappuniversity.com/bootcamp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Color Tokens
+        </a>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-white">
+              <span id="account">{account}</span>
+            </small>
+          </li>
+        </ul>
+      </nav>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex text-center">
+            <div className="content mr-auto ml-auto">
+              {/* form goes here */}
+            </div>
+          </main>
+        </div>
+        <hr />
+        <div className="row text-center">
+          {colors.map((color, key) => (
+            <div key={key} className="col-md-3 mb-3">
+              <div
+                style={{
+                  backgroundColor: color,
+                }}
+                className="token"
+              ></div>
+              <p>{color}</p>
+            </div>
+          ))}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
